a2d0e2cf79e1a78fae6ae9ad9c345c158d6a4241
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-06-07 20:23:53 -0700

    add image uploading

--- src/app/api/testing/[test]/route.ts
+++ src/app/api/testing/[test]/route.ts
@@ -1,6 +1,59 @@
+import { NextRequest, NextResponse } from "next/server";
+import { promises as fs } from "fs";
+import path from "path";
+
 export function POST(
   request: Request,
   { params }: { params: { test: string } }
 ) {
+  switch (params.test) {
+    case "craby":
+      return getResponse({
+        text: "Hi! This is craby!",
+      });
+    case "crab":
+      return getResponse({
+        text: "Hi! This is big boi crab!",
+      });
+    case "upload":
+      return handleFileUpload(request);
+  }
   return new Response("Hello, Next.js!");
+}
+
+export function getResponse(jsonData: any): Response {
+  return new Response(JSON.stringify(jsonData), {
+    headers: { "Content-Type": "application/json" },
+    status: 200,
+  });
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+async function handleFileUpload(req: Request) {
+  if (req.method !== "POST") {
+    return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
+  }
+
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file") as Blob;
+    const fileBuffer = Buffer.from(await file.arrayBuffer());
+    const filePath = path.join(
+      process.cwd(),
+      "public/uploads",
+      (file as any).name
+    );
+
+    await fs.mkdir(path.dirname(filePath), { recursive: true });
+    await fs.writeFile(filePath, fileBuffer);
+
+    return getResponse({ data: "File uploaded successfully" });
+  } catch (error) {
+    return getResponse({ data: "File upload failed" });
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
 
 /*
 ?
@@ -15,6 +15,10 @@ const defaultButtons: DefaultButton[] = [
     specification: "crab111",
     textOnButton: "Testing Button1",
   },
+  {
+    specification: "crab",
+    textOnButton: "Testing Button",
+  },
 ];
 
 // MAKE SURE TO PUT THE ACTUAL TEXT U WANT TO RENDER IN .text PROPERTY OF THE RESPONSE
@@ -26,7 +30,16 @@ const textRenderList: TextRender[] = [
     },
     clearTextTimeMS: 3000,
   },
+  {
+    button: {
+      specification: "craby1",
+      textOnButton: "Testing text button",
+    },
+    clearTextTimeMS: 3000,
+  },
 ];
+
+const imageUploadList: string[] = [];
 
 /*
 ?
@@ -42,6 +55,11 @@ interface TextRender {
   clearTextTimeMS: number;
 }
 
+interface ImageUploader {
+  button: DefaultButton;
+  clearTextTimeMS: number;
+}
+
 export default function Home() {
   function generateListWithBooleans(
     buttons: DefaultButton[]
@@ -50,12 +68,35 @@ export default function Home() {
     buttons.forEach((button) => {
       returnList.set(button.textOnButton, false);
     });
+
+    for (const text of textRenderList) {
+      returnList.set(text.button.textOnButton, false);
+    }
+
+    return returnList;
+  }
+
+  function generateListWithStrings(
+    buttons: DefaultButton[]
+  ): Map<string, string> {
+    const returnList: Map<string, string> = new Map();
+
+    for (const text of textRenderList) {
+      returnList.set(text.button.textOnButton, "Nothing here yet");
+    }
+
     return returnList;
   }
 
   const [clicked, setClicked] = useState<Map<string, boolean>>(
     generateListWithBooleans(defaultButtons)
   );
+
+  const [texts, setText] = useState<Map<string, string>>(
+    generateListWithStrings(defaultButtons)
+  );
+
+  const [clickedImageUpload, setClickedImageUpload] = useState<boolean>(false);
 
   function addToListWithBooleans(button: DefaultButton) {
     const resetClicked = new Map(clicked);
@@ -75,7 +116,7 @@ export default function Home() {
     console.log(jsonRes);
   }
 
-  async function handleClickBaseText(link: string) {
+  async function handleClickBaseText(link: string): Promise<string> {
     const res = await fetch("/api/testing/" + link, {
       method: "POST",
       headers: {
@@ -85,16 +126,42 @@ export default function Home() {
     });
 
     const jsonRes = await res.json();
-    const text = jsonRes.text;
+    const text: string = jsonRes.text;
+    return text;
+  }
+
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
+    if (event.target.files) {
+      setSelectedFile(event.target.files[0]);
+    }
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    if (!selectedFile) {
+      alert("Please select a file");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    const response = await fetch("/api/testing/upload", {
+      method: "POST",
+      body: formData,
+    });
   }
 
   return (
     <main className="p-10 flex flex-col">
-      <div className="w-full flex">
+      <div className="w-full flex border-2 p-2 rounded-2xl h-1/2 flex-grow flex-row flex-wrap">
         {defaultButtons.map((defaultButton, index) => (
           <button
             key={index}
-            className="w-40 h-20 border-2 border-black rounded-lg mx-5"
+            className="w-40 h-20 border-2 border-black rounded-lg mx-5 my-5"
             onClick={() => {
               handleClickBaseButton(defaultButton.specification);
               const newClicked = new Map(clicked);
@@ -112,6 +179,81 @@ export default function Home() {
               : defaultButton.textOnButton}
           </button>
         ))}
+      </div>
+
+      <div className="w-full mt-10 flex flex-grow border-2 p-5 rounded-2xl flex-row justify-between flex-wrap">
+        {textRenderList.map((textRender, index) => (
+          <div
+            key={index}
+            className="flex flex-col border-2 p-2 rounded-xl border-black m-2"
+          >
+            <a className="border-2 min-w-[500px] min-h-96 p-2 rounded-lg">
+              {texts.get(textRender.button.textOnButton) !== ""
+                ? texts.get(textRender.button.textOnButton) ===
+                  "TEXT_GENERATING_"
+                  ? "Waiting for text to come back"
+                  : texts.get(textRender.button.textOnButton)
+                : "Nothing here"}
+            </a>
+            <button
+              className="p-1 h-10 w-40 border-2 border-black rounded-lg mt-3"
+              onClick={async () => {
+                const newClicked = new Map(clicked);
+                newClicked.set(textRender.button.textOnButton, true);
+                setClicked(newClicked);
+
+                setTimeout(() => {
+                  const resetClicked = new Map(clicked);
+                  resetClicked.set(textRender.button.textOnButton, false);
+                  setClicked(resetClicked);
+                }, 300);
+
+                const newT = new Map(texts);
+                newT.set(textRender.button.textOnButton, "TEXT_GENERATING_");
+                setText(newT);
+
+                const newText = await handleClickBaseText(
+                  textRender.button.specification
+                );
+
+                const newTexts = new Map(texts);
+                newTexts.set(textRender.button.textOnButton, newText);
+                setText(newTexts);
+                setTimeout(() => {
+                  const updatedTexts = new Map(texts);
+                  updatedTexts.set(textRender.button.textOnButton, "");
+                  setText(updatedTexts);
+                }, textRender.clearTextTimeMS);
+              }}
+            >
+              {clicked.get(textRender.button.textOnButton)
+                ? "Clicked"
+                : textRender.button.textOnButton}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="w-full mt-10 flex flex-grow border-2 p-5 rounded-2xl flex-row justify-between flex-wrap">
+        <form onSubmit={handleSubmit}>
+          <input type="file" onChange={handleFileChange} ref={fileInputRef} />
+          <button
+            type="submit"
+            className="w-20 h-10 border-2 border-black rounded-lg"
+            onClick={() => {
+              setClickedImageUpload(true);
+
+              setTimeout(() => {
+                setClickedImageUpload(false);
+              }, 300);
+
+              if (fileInputRef.current) {
+                fileInputRef.current.value = "";
+              }
+            }}
+          >
+            {clickedImageUpload ? "Uploaded" : "Upload"}
+          </button>
+        </form>
       </div>
     </main>
   );
